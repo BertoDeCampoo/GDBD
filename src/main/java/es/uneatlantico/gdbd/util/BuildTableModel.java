1fac08a8689848c914501af1543d534438f670b9
albertogutierr <alberto.gutierrez@alumnos.uneatlantico.es>
2019-05-29 18:56:09 +0200

    Añadida documentación a BuildTableModel

--- src/main/java/es/uneatlantico/gdbd/util/BuildTableModel.java
+++ src/main/java/es/uneatlantico/gdbd/util/BuildTableModel.java
@@ -9,6 +9,12 @@ import javax.swing.table.DefaultTableModel;
 
 public class BuildTableModel {
 	
+	/**
+	 * Constructs a default table model for a JTable component given a result set
+	 * @param rs  SQL query result set
+	 * @return  the DefaultTableModel
+	 * @throws SQLException  if the result set is invalid
+	 */
 	public static DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
 
 	    ResultSetMetaData metaData = rs.getMetaData();
