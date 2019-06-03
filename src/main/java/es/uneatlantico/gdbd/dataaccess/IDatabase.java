9ca77a7072031e76269231fbefae73a23b907ea8
albertogutierr <alberto.gutierrez@alumnos.uneatlantico.es>
2019-06-04 00:40:00 +0200

    Actualizada interfaz IDatabase para que lance excepciones

--- src/main/java/es/uneatlantico/gdbd/dataaccess/IDatabase.java
+++ src/main/java/es/uneatlantico/gdbd/dataaccess/IDatabase.java
@@ -42,25 +42,29 @@ public interface IDatabase {
 	 * Obtains the name of the tables on the given database
 	 * @param  databaseName  the name of the database from wich to get the tables
 	 * @return the name of the tables of the database
+	 * @throws SQLException  if it is not possible to get the tables of the selected database
 	 */
-	public List<Table> getTables();
+	public List<Table> getTables() throws SQLException;
 	
 	/**
 	 * Obtains the columns of the given table
 	 * @param tableName  name of the table from which to retrieve the columns
 	 * @return a list with the names of the columns of the selected database
+	 * @throws SQLException  if it isn't possible to obtain the columns
 	 */
-	public List<Column> getColumns(String tableName);
+	public List<Column> getColumns(String tableName) throws SQLException;
 	
 	/**
 	 * Obtains the name of the tables and the columns of each table
 	 * @return  a Database entity with all the information of the current database
+	 * @throws SQLException  if it isn't possible to get the database information
 	 */
-	public Database getDatabaseInformation();
+	public Database getDatabaseInformation() throws SQLException;
 	
 	/**
 	 * Obtains a list with the databases on this server
 	 * @return  a list with the databases on the server
+	 * @throws SQLException  if the databases can't be obtained
 	 */
-	public List<String> getDatabasesOnThisServer();
+	public List<String> getDatabasesOnThisServer() throws SQLException;
 }
