bebaaa994deb86a214aaa449fbfda4392cc19033
albertogutierr <alberto.gutierrez@alumnos.uneatlantico.es>
2019-06-03 19:58:41 +0200

    Añadida función getDatabaseReportFilename() a Configuracion
    Mejoras de documentación Java Doc

--- src/main/java/es/uneatlantico/gdbd/util/Configuration.java
+++ src/main/java/es/uneatlantico/gdbd/util/Configuration.java
@@ -1,6 +1,5 @@
 package es.uneatlantico.gdbd.util;
 
-import java.io.FileInputStream;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
@@ -8,7 +7,7 @@ import org.apache.logging.log4j.Logger;
 
 /**
  * Class to load and retrieve application properties
- *
+ * @author Alberto Gutiérrez Arroyo
  */
 public class Configuration {
 	
@@ -16,18 +15,39 @@ public class Configuration {
 	private static String configFile = "/config.properties";
 	private static Properties properties = null;
 	
+	/**
+	 * Obtains the default name for the SQLite database
+	 * @return  the name for the SQLite database
+	 */
 	public static String getDefaultSQLiteFilename(){
 		if (properties == null)
 			initializeProperties();
 		return properties.getProperty("DEFAULT_SQLITE_FILENAME");
 	}
 	
+	/**
+	 * Obtains the default report name (The file the user will get once generated) without the file extension
+	 * @return  the default name of the exported report without extension
+	 */
 	public static String getDefaultReportFilename(){
 		if (properties == null)
 			initializeProperties();
 		return properties.getProperty("DEFAULT_REPORT_NAME");
 	}
 	
+	/**
+	 * Obtains the Jasper Reports database report (A JRXML file) file name
+	 * @return  the name of the database report file
+	 */
+	public static String getDatabaseReportFilename() {
+		if (properties == null)
+			initializeProperties();
+		return properties.getProperty("DATABASE_REPORT_FILENAME");
+	}
+	
+	/**
+	 * Initializes the properties
+	 */
 	private static void initializeProperties(){
 		try{
 			properties = new Properties();
