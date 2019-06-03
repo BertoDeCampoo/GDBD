9db036b22fa5b5f4329b8c95b324fd5810b18878
albertogutierr <alberto.gutierrez@alumnos.uneatlantico.es>
2019-06-03 21:34:56 +0200

    Corregido PDFReport para que no falle cuando se ejecute desde el .jar

--- src/main/java/es/uneatlantico/es/gdbd/reports/PDFReport.java
+++ src/main/java/es/uneatlantico/es/gdbd/reports/PDFReport.java
@@ -1,6 +1,5 @@
 package es.uneatlantico.es.gdbd.reports;
 
-import java.io.File;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,11 +8,11 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
 
 /**
  * Class which implements the IReport interface and can generate Portable Document Format (PDF) files
@@ -21,17 +20,13 @@ import net.sf.jasperreports.engine.JasperReport;
  */
 public class PDFReport implements IReport {
 
-	private static final Logger logger = LogManager.getLogger(PDFReport.class); 
+	private static final Logger logger = LogManager.getLogger(PDFReport.class);
 	
 	@Override
 	public String export(String reportFile, String exportPath, Connection dataSourceConnection) throws Exception {
 		exportPath += ".pdf";
 		logger.log(Level.INFO, "Generando informe PDF en " + exportPath + "...");
-		File file = new File(getClass().getClassLoader().getResource(reportFile).getFile());
-		
-		String filePath = file.getCanonicalPath();
-		
-		JasperReport jasperReport = JasperCompileManager.compileReport(filePath);
+		JasperReport jasperReport = (JasperReport) JRLoader.loadObject(PDFReport.this.getClass().getResource(reportFile));
 		
 		// Parameters for report
 		Map<String, Object> parameters = new HashMap<String, Object>();
